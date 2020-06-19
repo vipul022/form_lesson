38528e42093065abe8135ac19cce6e9b8f2157ed
vipul022 <vipul.seth022@gmail.com>
2020-06-20 09:13:54 +1000

    created routes, movies controller and views files

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,17 @@
+class MoviesController < ApplicationController
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def new
+
+    end
+
+    def show
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/movies", to: "movies#index", as: "movies"
   post "/movies", to: "movies#create"
-  get "movies/new", to: "movies#new", as: "new_movie"
+  get "/movies/new", to: "movies#new", as: "new_movie"
+  get "/movies/:id", to: "movies#show", as: "movie"
 end
