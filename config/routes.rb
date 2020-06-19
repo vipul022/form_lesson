90506a7ba37bfdcb661b81383e9c2869a085db8a
vipul022 <vipul.seth022@gmail.com>
2020-06-20 08:58:16 +1000

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/movies", to: "movies#index", as: "movies"
+  post "/movies", to: "movies#create"
+  get "movies/new", to: "movies#new", as: "new_movie"
+end
