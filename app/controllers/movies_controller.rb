b3a8573e185116ea8957fba012110c27eb3a2f3d
vipul022 <vipul.seth022@gmail.com>
2020-06-20 23:04:58 +1000

    created form for creating a new movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,24 @@
 class MoviesController < ApplicationController
     def index
-
+       @movies = session[:movies]
     end
 
     def create
+      # puts params[:movie]
+      if session[:movies] == nil
+          session[:movies] = []
+      end    
+      
 
+      session[:movies].push(params[:movie])
+
+      redirect_to movies_path
+     
     end
 
     def new
 
-    end
+    end 
 
     def show
 
